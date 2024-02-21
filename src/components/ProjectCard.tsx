3ad9821c98eba63e5480e6189f11f35623d8da1e
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-20 22:06:16 -0500

    Add files via upload

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+export default function ProjectCard({ url, title, description, image, delay, gradient }:{ url: string, title: string, description: string, image: string, delay: number, gradient: string}) {
+  return (
+    <>
+    
+    </>
+  );
+}
