b77ec95973cd7d01b50aa5d9f26055037c3fd320
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-24 00:07:46 -0500

    Delete src directory

--- src/components/ProjectCard.tsx
+++
@@ -1,9 +0,0 @@
-import { motion } from "framer-motion";
-
-export default function ProjectCard({ url, title, description, image, delay, gradient }: { url: string, title: string, description: string, image: string, delay: number, gradient: string }) {
-  return (
-    <>
-
-    </>
-  );
-}
